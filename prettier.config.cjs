6c14e6989c6a26bfc1101f47cf8e245f91d7be4b
jillusc <jillus@hotmail.com>
2025-11-13 17:29:28 +0100

    Initial commit: with Prettier config

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true, // add semicolons at the end of statements
+  singleQuote: false, // use double quotes
+  trailingComma: "es5", // include trailing commas where valid in ES5 (objects, arrays, etc.)
+  tabWidth: 2, // 2 spaces per tab
+  printWidth: 80, // wrap lines longer than 80 characters
+  bracketSpacing: true, // put spaces between brackets in object literals
+  arrowParens: "always", // always include parentheses around arrow function params
+};
